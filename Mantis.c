1cc1e7b7bc25f9bcdaa3e3f2960004a963c36486
loraine <lrnbtriz.gmt@gmail.com>
2026-03-27 18:47:25 +0800

    Refactor Mantis.c and add twoPlayerMode feature
    Updated the main function and added twoPlayerMode setting.

--- Mantis.c
+++ Mantis.c
@@ -2,23 +2,16 @@
  *  Description     : The main heart of the code used for calling out functions and creating variables.
  *  Author/s        : Agamata, Loraine Beatriz C.
  *                    Lapuz, Dale Lucian M.
- *  Section         : 
- *  Last Modified   : March 15, 2026
+ *  Section         : S12A & S22A
+ *  Last Modified   : March 27, 2026
  *  Acknowledgments : <list of references used in the making of this project>
  ******************************************************************************/
 
 /* ----- preprocessor directives ----- */
 #include <stdio.h>
 #include "Essentials\defs.h"
-#include "Essentials\helpers\helpers_1.c"
-#include "Essentials\helpers\helpers_2.c"
+#include "Essentials\helpers\helpers.c"
 #include "Code-Proper\menu.c"
-
-
-
-/* ----- definitions (i.e., constants, typedefs, structs) ----- */
-
-
 
 /* ----- function implementations ----- */
 int main()
@@ -28,8 +21,9 @@ int main()
     initRandom();
 
     GameSettings settings;
-    settings.winningPts    = WINNING_SCORE;
+    settings.winningPts = WINNING_SCORE;
     settings.shufflingSeed = randomInt();
+    settings.twoPlayerMode = 0; //BONUS: default off
 
     GameData game;
 
@@ -70,7 +64,3 @@ int main()
 
     return 0;
 }
-
-
-
-
